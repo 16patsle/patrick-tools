3362c13d47f31168feb6a0bc3d633db2dfb6cfda
Patrick Sletvold <patricksletvold@hotmail.com>
2021-01-22 11:49:13 +0100

    Format binary calculator file

--- src/calculators/BinaryDecimalConverter.js
+++ src/calculators/BinaryDecimalConverter.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
-import convertBinaryToDecimal from '../utils/convertBinaryToDecimal.mjs';
-import convertDecimalToBinary from '../utils/convertDecimalToBinary.mjs';
+import convertBinaryToDecimal from '../utils/convertBinaryToDecimal.mjs'
+import convertDecimalToBinary from '../utils/convertDecimalToBinary.mjs'
 
 const BinaryDecimalConverter = () => {
   const [binary, setBinary] = useState('')
@@ -10,37 +10,51 @@ const BinaryDecimalConverter = () => {
   const updateBinary = e => {
     const value = e.target.value
 
-    setBinary(value);
+    setBinary(value)
     setDecimal(convertBinaryToDecimal(value) || '')
   }
 
   const updateDecimal = e => {
     const value = e.target.value
 
-    setDecimal(value);
+    setDecimal(value)
     setBinary(convertDecimalToBinary(value) || '')
   }
 
-  return <div>
-    <h2 className="text-2xl">Convert to and from binary</h2>
-    <label className="block m-2">
-      Binary: 
-      <input type="text" pattern="[01]+" value={binary} onChange={updateBinary} onKeyPress={e=>{
-        if(e.key !== "1" && e.key !== "0" && e.key !== "-") {
-          e.preventDefault()
-        }
-      }} className="shadow-md bg-gray-50 border-gray-200 border-2 rounded-md mx-2 p-1" />
-    </label>
-    <label className="block m-2">
-      Decimal:
-      
-      <input type="number" value={decimal} onChange={updateDecimal} onKeyPress={e=>{
-        if(e.key === "." || e.key === ",") {
-          e.preventDefault()
-        }
-      }} className="shadow-md bg-gray-50 border-gray-200 border-2 rounded-md mx-2 p-1" />
-    </label>
-  </div>
+  return (
+    <div>
+      <h2 className="text-2xl">Convert to and from binary</h2>
+      <label className="block m-2">
+        Binary:
+        <input
+          type="text"
+          pattern="[01]+"
+          value={binary}
+          onChange={updateBinary}
+          onKeyPress={e => {
+            if (e.key !== '1' && e.key !== '0' && e.key !== '-') {
+              e.preventDefault()
+            }
+          }}
+          className="shadow-md bg-gray-50 border-gray-200 border-2 rounded-md mx-2 p-1"
+        />
+      </label>
+      <label className="block m-2">
+        Decimal:
+        <input
+          type="number"
+          value={decimal}
+          onChange={updateDecimal}
+          onKeyPress={e => {
+            if (e.key === '.' || e.key === ',') {
+              e.preventDefault()
+            }
+          }}
+          className="shadow-md bg-gray-50 border-gray-200 border-2 rounded-md mx-2 p-1"
+        />
+      </label>
+    </div>
+  )
 }
 
 export default BinaryDecimalConverter
