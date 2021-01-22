a654e63410c3f7dea51a937e6b81dd86ce5d535a
Patrick Sletvold <patricksletvold@hotmail.com>
2021-01-22 10:05:40 +0100

    Implement converting decimal to binary

--- src/utils/convertDecimalToBinary.mjs
+++ src/utils/convertDecimalToBinary.mjs
@@ -1,8 +1,34 @@
+const findLargestFittingPowerOfTwo = number => {
+  let factor = 1
+  while (factor * 2 <= number) {
+    factor *= 2
+  }
+  return factor
+}
+
 /**
  * Convert decimal to binary number
  * @param {string} decimalStr - The string representation of the decimal number.
  * @param {boolean} twosComplement - Whether to convert negative numbers using two's complement
  */
 export default function convertDecimalToBinary(decimalStr, twosComplement) {
-  
+  if (!/^[0-9]+$/.test(decimalStr)) {
+    return false
+  }
+
+  let number = parseInt(decimalStr, 10)
+  let binary = ''
+  let factor = findLargestFittingPowerOfTwo(number)
+
+  while (factor >= 1) {
+    if (factor <= number) {
+      binary += '1'
+      number -= factor
+    } else {
+      binary += '0'
+    }
+    factor /= 2
+  }
+
+  return binary
 }
