f218e12650b231094362b30238543bc47335830e
Patrick Sletvold <patricksletvold@hotmail.com>
2021-01-23 18:49:05 +0100

    Configure CSS optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-const path = require("path");
+const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const createConfig = isDevelopment => ({
   entry: path.resolve(__dirname, 'src/index.js'),
   //target: isDevelopment ? 'web' : 'browserslist',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -26,16 +28,37 @@ const createConfig = isDevelopment => ({
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: [
+          {
+            loader: ExtractCssChunks.loader,
+            options: {
+              esModule: true,
+              hmr: isDevelopment,
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+        ],
       },
     ],
+  },
+  optimization: {
+    minimize: !isDevelopment,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
     }),
+    new ExtractCssChunks({
+      filename: isDevelopment ? '[name].css' : '[name].[contenthash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[contenthash].css',
+    }),
     //isDevelopment && new ReactRefreshWebpackPlugin(),
   ].filter(Boolean),
 })
 
-module.exports = (env, argv) => createConfig(argv.mode === 'development')
+module.exports = (env, argv) => createConfig(!env.production || argv.mode === 'development')
