12afe6768836d0e58b6e9b95962bbbedf1ea360a
Patrick Sletvold <patricksletvold@hotmail.com>
2021-01-23 16:07:55 +0100

    Add margin to header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BinaryDecimalConverter from './calculators/BinaryDecimalConverter'
 const App = () => {
   return (
     <div className="m-2">
-      <h1 className="text-3xl">Patrick's Tools</h1>
+      <h1 className="text-3xl mb-2">Patrick's Tools</h1>
       <BinaryDecimalConverter />
     </div>
   )
