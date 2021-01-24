fee5d377fea1ac313a49aeb69c476f4bda9239ae
Patrick Sletvold <patricksletvold@hotmail.com>
2021-01-24 11:31:03 +0100

    Implement conversion from text to ASCII

---
+++ src/utils/convertTextToASCIIBinary.js
@@ -0,0 +1,22 @@
+import convertDecimalToBinary from './convertDecimalToBinary.js'
+
+/**
+ * Convert text to ASCII binary
+ * @param {string} textStr - The string to convert.
+ * @param {boolean} [spaces] - Separate bytes with spaces.
+ * @returns {string|false} The converted text, or false if failed.
+ */
+export default function convertTextToASCIIBinary(textStr, spaces = true) {
+  // Split string into characters
+  return textStr.split('')
+    // Get char code
+    .map(val=> val.charCodeAt(0))
+    // Remove non-ASCII chars
+    .filter(val => val <= 255)
+    // Convert to decimal
+    .map(val => convertDecimalToBinary(String(val)))
+    // If not false, pad with 0's to reach 8 bit
+    .map(val => val !== false && val.padStart(8, '0'))
+    // Join with spaces if enabled
+    .join(spaces ? ' ': '')
+}
---
+++ tests/convertTextToASCIIBinary.mjs
@@ -0,0 +1,19 @@
+import test from 'ava'
+import convertTextToASCIIBinary from '../src/utils/convertTextToASCIIBinary.js'
+
+
+test('silently ignores non-ASCII characters', t => {
+  t.is(convertTextToASCIIBinary('A∆'), '01000001')
+})
+
+test('converts a single character into binary ASCII', t=> {
+  t.is(convertTextToASCIIBinary('A'), '01000001')
+})
+
+test('converts a longer string into binary ASCII', t=> {
+  t.is(convertTextToASCIIBinary('ABCDE'), '01000001 01000010 01000011 01000100 01000101')
+})
+
+test('converts a longer string into binary ASCII, without spaces', t=> {
+  t.is(convertTextToASCIIBinary('ABCDE', false), '0100000101000010010000110100010001000101')
+})
