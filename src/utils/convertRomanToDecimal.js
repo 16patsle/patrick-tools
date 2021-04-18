150cdcc11ade74bdb0d537db5b889b098c8af4c6
Patrick Sletvold <patricksletvold@hotmail.com>
2021-04-18 15:56:12 +0200

    Implement convert roman to decimal

--- src/utils/convertRomanToDecimal.js
+++ src/utils/convertRomanToDecimal.js
@@ -8,5 +8,38 @@ export default function convertRomanToDecimal(romanStr) {
     return false
   }
 
-  return false
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  // split array, convert to numbers, reverse to start at right end
+  const arr = romanStr
+    .split('')
+    .map(val => map[val])
+    .reverse()
+
+  let lastNumber = 0
+  let number = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i]
+
+    if (lastNumber > 0 && current < lastNumber) {
+      // If smaller than last number, subtract (e.g. IV or IX)
+      number -= current
+    } else {
+      // Else add (e.g. III, XI, CV or anything else)
+      number += current
+    }
+
+    lastNumber = current
+  }
+
+  return number
 }
