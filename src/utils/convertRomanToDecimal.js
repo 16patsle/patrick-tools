c3a281f42b244c0e819c971e6815805b5a50da5e
Patrick Sletvold <patricksletvold@hotmail.com>
2021-04-18 14:57:56 +0200

    Add basic implementation of convertRomanToDecimal

---
+++ src/utils/convertRomanToDecimal.js
@@ -0,0 +1,12 @@
+/**
+ * Convert Roman numerals to decimal
+ * @param {string} romanStr - The string representation of the Roman numeral.
+ * @returns {number|false} The converted decimal number, or false if failed.
+ */
+export default function convertRomanToDecimal(romanStr) {
+  if (!/^[IVXLCDM]+$/.test(romanStr)) {
+    return false
+  }
+
+  return false
+}
