a4afc42886a1a6124183cef528f6e4a26f229b31
Patrick Sletvold <patricksletvold@hotmail.com>
2021-12-20 13:04:07 +0100

    Add anchor variant of button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,6 +11,23 @@ type ButtonProps = {
   disabled?: boolean
 }
 
+type AnchorButtonProps = {
+  /**
+   * Output a element with href instead of button
+   */
+  href?: string
+  /**
+   * Callback on button click
+   */
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void
+  /**
+   * Prompt for download on click
+   */
+  download?: boolean
+}
+
+const className = "w-full h-9 shadow-md active:shadow-lg active:shadow-yellow-500 bg-gray-50 active:bg-yellow-500 uppercase text-sm text-center font-semibold text-gray-500 active:text-gray-50 focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 border-gray-200 active:border-yellow-500 border-2 rounded-md p-1 my-3"
+
 export const Button: FunctionComponent<ButtonProps> = ({
   children,
   onClick,
@@ -18,11 +35,27 @@ export const Button: FunctionComponent<ButtonProps> = ({
 }) => {
   return (
     <button
-      className="w-full h-9 shadow-md active:shadow-lg active:shadow-yellow-500 bg-gray-50 active:bg-yellow-500 uppercase text-sm font-semibold text-gray-500 active:text-gray-50 focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 border-gray-200 active:border-yellow-500 border-2 rounded-md p-1 my-3"
+      className={className}
       onClick={onClick}
       disabled={disabled}
     >
       {children}
     </button>
   )
+}
+
+export const AnchorButton: FunctionComponent<AnchorButtonProps> = ({
+  children,
+  href,
+  onClick,
+  download = false,
+}) => {
+  return (<a
+    className={className}
+    href={href}
+    onClick={onClick}
+    download={download}
+  >
+    {children}
+  </a>)
 }
