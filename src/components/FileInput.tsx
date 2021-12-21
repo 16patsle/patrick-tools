6d752589cd463dc87ed6e9ab8352ce5a52ec7644
Patrick Sletvold <patricksletvold@hotmail.com>
2021-12-21 12:31:45 +0100

    Add special styling for file input button

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -33,18 +33,14 @@ export const FileInput = ({
       <div className="col-span-1 text-gray-500 uppercase text-sm font-semibold">
         {children}
       </div>
-      <div
-        className={`w-full h-auto flex items-center shadow-md bg-gray-50 hover:bg-gray-100 caret-yellow-500 focus-within:ring-2 focus-within:ring-yellow-500 focus-within:ring-offset-2 border-gray-200 border-2 rounded-md p-2${
+      <input
+        type="file"
+        accept={accept}
+        onChange={e => onChange(e.target.files?.[0] ?? null, e)}
+        className={`w-full h-auto file:h-9 file:shadow-md active:file:shadow-lg active:file:shadow-yellow-500 file:bg-gray-50 active:file:bg-yellow-500 hover:file:bg-gray-100 uppercase file:uppercase text-sm font-semibold file:font-semibold text-gray-500 file:text-gray-500 active:file:text-gray-50 focus-visible:ring-2 focus-visible:ring-yellow-500 focus-visible:ring-offset-2 focus:file:ring-2 focus:file:ring-yellow-500 focus:file:ring-offset-2 focus:outline-none file:border-gray-200 active:file:border-yellow-500 file:border-solid file:border-2 rounded-md file:rounded-md p-1 file:py-1 file:px-2 -mx-1 file:mr-2${
           className ? ` ${className}` : ''
         }`}
-      >
-        <input
-          type="file"
-          accept={accept}
-          onChange={e => onChange(e.target.files?.[0] ?? null, e)}
-          className="focus:outline-none"
-        />
-      </div>
+      />
     </label>
   )
 }
