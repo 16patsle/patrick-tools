e01567e162643ac55a24d02a447d200d1c10ab97
Patrick Sletvold <patricksletvold@hotmail.com>
2022-01-20 21:12:52 +0100

    Add display titles to temperature units

--- src/data/temperature.ts
+++ src/data/temperature.ts
@@ -1,34 +1,42 @@
 export const temperature = [
   {
     name: 'kelvin',
+    display: 'Kelvin',
     symbol: 'K',
   },
   {
     name: 'celsius',
+    display: 'Celsius',
     symbol: '°C',
   },
   {
     name: 'fahrenheit',
+    display: 'Fahrenheit',
     symbol: '°F',
   },
   {
     name: 'rankine',
+    display: 'Rankine',
     symbol: '°R',
   },
   {
     name: 'delisle',
+    display: 'Delisle',
     symbol: '°D',
   },
   {
     name: 'newton',
+    display: 'Newton',
     symbol: '°N',
   },
   {
     name: 'reaumur',
+    display: 'Réaumur',
     symbol: '°Ré',
   },
   {
     name: 'romer',
+    display: 'Rømer',
     symbol: '°Rø',
   },
 ]
