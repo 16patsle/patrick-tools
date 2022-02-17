aa3901a6c69f3c4d6f7535d6c9d036edc4e1029b
Patrick Sletvold <patricksletvold@hotmail.com>
2022-02-17 19:22:22 +0100

    Generalize UI for TemperatureConverter

--- src/calculators/TemperatureConverter.tsx
+++ src/calculators/TemperatureConverter.tsx
@@ -1,84 +1,16 @@
-import { useState } from 'react'
-import Input from '../components/Input'
-import { Heading2 } from '../components/Heading2'
-import { useConverterState } from '../utils/useConverterState'
 import { convertTemperature } from '../utils/convertTemperature'
-import { Select } from '../components/Select'
+import { SelectConverter } from '../components/SelectConverter'
 
 import { temperature } from '../data/temperature'
-import Big from 'big.js'
-
-const UnitSelect = ({
-  unit: [value, setValue],
-}: {
-  unit: [string, (value: string) => void]
-}) => {
-  return (
-    <Select value={value} onChange={setValue}>
-      {temperature.map(({ name, display, symbol }) => (
-        <option key={name} value={name}>
-          {display} ({symbol})
-        </option>
-      ))}
-    </Select>
-  )
-}
 
 const TemperatureConverter = () => {
-  const [valueFirst, setValueFirst, recalculateFromValueFirst] =
-    useConverterState(new Big(0), (value, from, to): void =>
-      setValueSecond(convertTemperature(value, from, to, 1))
-    )
-  const [valueSecond, setValueSecond, recalculateFromValueSecond] =
-    useConverterState(new Big(0), (value, from, to): void =>
-      setValueFirst(convertTemperature(value, from, to, 1))
-    )
-
-  const [unitFirst, setUnitFirst] = useState('celsius')
-  const [unitSecond, setUnitSecond] = useState('fahrenheit')
-  const unitFirstData = temperature.find(value => value.name === unitFirst)
-  const unitSecondData = temperature.find(value => value.name === unitSecond)
-
   return (
-    <div className="max-w-lg">
-      <Heading2>Convert temperature</Heading2>
-      <UnitSelect
-        unit={[
-          unitFirst,
-          newUnitFirst => {
-            setUnitFirst(newUnitFirst)
-            recalculateFromValueFirst(valueFirst, newUnitFirst, unitSecond)
-          },
-        ]}
-      />
-      <Input
-        type="number"
-        value={valueFirst}
-        onChange={val =>
-          recalculateFromValueFirst(new Big(val || 0), unitFirst, unitSecond)
-        }
-      >
-        {unitFirstData?.display} ({unitFirstData?.symbol})
-      </Input>
-      <UnitSelect
-        unit={[
-          unitSecond,
-          newUnitSecond => {
-            setUnitSecond(newUnitSecond)
-            recalculateFromValueFirst(valueFirst, unitFirst, newUnitSecond)
-          },
-        ]}
-      />
-      <Input
-        type="number"
-        value={valueSecond}
-        onChange={val =>
-          recalculateFromValueSecond(new Big(val || 0), unitSecond, unitFirst)
-        }
-      >
-        {unitSecondData?.display} ({unitSecondData?.symbol})
-      </Input>
-    </div>
+    <SelectConverter
+      convertFunction={convertTemperature}
+      options={temperature}
+      initialUnits={['celsius', 'fahrenheit']}
+      title="Convert temperature"
+    />
   )
 }
 
---
+++ src/components/SelectConverter.tsx
@@ -0,0 +1,105 @@
+import { useState } from 'react'
+import Input from '../components/Input'
+import { Heading2 } from '../components/Heading2'
+import { useConverterState } from '../utils/useConverterState'
+import { Select } from '../components/Select'
+import Big, { BigSource } from 'big.js'
+
+type SelectOptions = {
+  name: string
+  display: string
+  symbol: string
+}[]
+
+const UnitSelect = ({
+  unit: [value, setValue],
+  options,
+}: {
+  unit: [string, (value: string) => void]
+  options: SelectOptions
+}) => {
+  return (
+    <Select value={value} onChange={setValue}>
+      {options.map(({ name, display, symbol }) => (
+        <option key={name} value={name}>
+          {display} ({symbol})
+        </option>
+      ))}
+    </Select>
+  )
+}
+
+export const SelectConverter = <T extends string>({
+  convertFunction,
+  options,
+  initialUnits: [initialFirstUnit, initialSecondUnit],
+  title,
+}: {
+  convertFunction(
+    value: BigSource,
+    from: T,
+    to: T,
+    dp?: number | false
+  ): false | Big
+  options: SelectOptions
+  initialUnits: [T, T]
+  title: string
+}) => {
+  const [valueFirst, setValueFirst, recalculateFromValueFirst] =
+    useConverterState(new Big(0), (value, from, to): void =>
+      setValueSecond(convertFunction(value, from, to, 1))
+    )
+  const [valueSecond, setValueSecond, recalculateFromValueSecond] =
+    useConverterState(new Big(0), (value, from, to): void =>
+      setValueFirst(convertFunction(value, from, to, 1))
+    )
+
+  const [unitFirst, setUnitFirst] = useState(initialFirstUnit)
+  const [unitSecond, setUnitSecond] = useState(initialSecondUnit)
+  const unitFirstData = options.find(value => value.name === unitFirst)
+  const unitSecondData = options.find(value => value.name === unitSecond)
+
+  return (
+    <div className="max-w-lg">
+      <Heading2>{title}</Heading2>
+      <UnitSelect
+        unit={[
+          unitFirst,
+          newUnitFirst => {
+            setUnitFirst(newUnitFirst as T)
+            recalculateFromValueFirst(valueFirst, newUnitFirst, unitSecond)
+          },
+        ]}
+        options={options}
+      />
+      <Input
+        type="number"
+        value={valueFirst}
+        onChange={val =>
+          recalculateFromValueFirst(new Big(val || 0), unitFirst, unitSecond)
+        }
+      >
+        {unitFirstData?.display} ({unitFirstData?.symbol})
+      </Input>
+      <UnitSelect
+        unit={[
+          unitSecond,
+          newUnitSecond => {
+            setUnitSecond(newUnitSecond as T)
+            recalculateFromValueFirst(valueFirst, unitFirst, newUnitSecond)
+          },
+        ]}
+        options={options}
+      />
+      <Input
+        type="number"
+        value={valueSecond}
+        onChange={val =>
+          recalculateFromValueSecond(new Big(val || 0), unitSecond, unitFirst)
+        }
+      >
+        {unitSecondData?.display} ({unitSecondData?.symbol})
+      </Input>
+    </div>
+  )
+}
