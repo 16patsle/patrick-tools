f181e09a4ba08bc80dcba973551a86edb346d010
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-03 12:16:04 +0200

    Add display names to length units

--- src/data/length.ts
+++ src/data/length.ts
@@ -1,122 +1,152 @@
 export const length = [
   {
     name: 'picometer',
+    display: 'Picometer',
     symbol: 'pm',
   },
   {
     name: 'angstrom',
+    display: 'Angstrom',
     symbol: 'Å',
   },
   {
     name: 'nanometer',
+    display: 'Nanometer',
     symbol: 'nm',
   },
   {
     name: 'micrometer',
+    display: 'Micrometer',
     symbol: 'µm',
   },
   {
     name: 'millimeter',
+    display: 'Millimeter',
     symbol: 'mm',
   },
   {
     name: 'centimeter',
+    display: 'Centimeter',
     symbol: 'cm',
   },
   {
     name: 'meter',
+    display: 'Meter',
     symbol: 'm',
   },
   {
     name: 'kilometer',
+    display: 'Kilometer',
     symbol: 'km',
   },
   {
     name: 'gigameter',
+    display: 'Gigameter',
     symbol: 'Gm',
   },
   {
     name: 'inch',
+    display: 'Inch',
     symbol: 'in',
   },
   {
     name: 'foot',
+    display: 'Foot',
     symbol: 'ft',
   },
   {
     name: 'yard',
+    display: 'Yard',
     symbol: 'yd',
   },
   {
+    name: 'fathom',
+    display: 'Fathom',
+    symbol: 'fth',
+  },
+  {
     name: 'chain',
+    display: 'Chain',
     symbol: 'ch',
   },
   {
     name: 'rod',
+    display: 'Rod',
     symbol: 'rd',
   },
   {
     name: 'link',
+    display: 'Link',
     symbol: 'li',
   },
   {
     name: 'furlong',
+    display: 'Furlong',
     symbol: 'fur',
   },
   {
     name: 'mile',
+    display: 'Mile',
     symbol: 'mi',
   },
   {
     name: 'nautical mile',
+    display: 'Nautical mile',
     symbol: 'nmi',
   },
   {
     name: 'light-second',
+    display: 'Light-second',
     symbol: 'ls',
   },
   {
     name: 'light-minute',
+    display: 'Light-minute',
     symbol: 'lm',
   },
   {
     name: 'light-hour',
+    display: 'Light-hour',
     symbol: 'lh',
   },
   {
     name: 'light-day',
+    display: 'Light-day',
     symbol: 'ld',
   },
   {
     name: 'light-week',
+    display: 'Light-week',
     symbol: 'lw',
   },
   {
     name: 'light-year',
+    display: 'Light-year',
     symbol: 'ly',
   },
   {
     name: 'astronomical unit',
+    display: 'Astronomical unit',
     symbol: 'au',
   },
   {
     name: 'parsec',
+    display: 'Parsec',
     symbol: 'pc',
   },
   {
     name: 'kiloparsec',
+    display: 'Kiloparsec',
     symbol: 'kpc',
   },
   {
     name: 'megaparsec',
+    display: 'Megaparsec',
     symbol: 'Mpc',
   },
   {
     name: 'gigaparsec',
+    display: 'Gigaparsec',
     symbol: 'Gpc',
-  },
-  {
-    name: 'fathom',
-    symbol: 'fth',
   },
 ]
