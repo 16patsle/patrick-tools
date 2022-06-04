194e1b49daea17e65ab3015039c85933cf8e885f
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-04 12:16:58 +0200

    Switch out lodash.debounce for just-debounce-it

--- src/utils/useDebounceCallback.ts
+++ src/utils/useDebounceCallback.ts
@@ -1,5 +1,5 @@
 import { useCallback } from 'react'
-import debounce from 'lodash.debounce'
+import debounce from 'just-debounce-it'
 
 export const useDebouncedCallback = (callback: (...args: any[]) => any) =>
   useCallback(debounce(callback, 200), [])
