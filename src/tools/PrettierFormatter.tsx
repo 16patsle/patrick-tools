473140b258459d01ff32f6f6ef7ea93dd145f1b8
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-04 14:28:23 +0200

    Add language selector to prettier format

--- src/tools/PrettierFormatter.tsx
+++ src/tools/PrettierFormatter.tsx
@@ -1,11 +1,14 @@
 import { useCallback, useState } from 'react'
 import { Button } from '../components/Button'
+import { Label } from '../components/Label'
 import { ErrorNotice } from '../components/Notice'
+import { Select } from '../components/Select'
 import TextArea from '../components/TextArea'
-import { prettierFormat } from '../utils/tools/prettierFormat'
+import { parsers, prettierFormat } from '../utils/tools/prettierFormat'
 
 export const PrettierFormatter = () => {
   const [code, setCode] = useState('')
+  const [language, setLanguage] = useState('babel')
   const [error, setError] = useState('')
   const [isFormatting, setIsFormatting] = useState(false)
 
@@ -33,6 +36,18 @@ export const PrettierFormatter = () => {
       <TextArea value={code} onChange={setCode}>
         Code
       </TextArea>
+      <Label text="Language">
+        <Select
+          value={language}
+          onChange={value => setLanguage(value)}
+        >
+          {parsers.map((parser) => (
+              <option key={parser.id} value={parser.id}>
+                {parser.name}
+              </option>
+            ))}
+        </Select>
+      </Label>
       {isFormatting && <p>Converting...</p>}
       {error && (
         <ErrorNotice>
--- src/utils/tools/prettierFormat.ts
+++ src/utils/tools/prettierFormat.ts
@@ -28,3 +28,63 @@ export const prettierFormat = (code: string): Promise<string> =>
       setTimeout(() => reject('Formatting took too long, try again.'), 10000)
     ),
   ])
+
+export const parsers = [
+  {
+    id: 'babel',
+    name: 'JavaScript',
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  {
+    id: 'babel-ts',
+    name: 'TypeScript',
+    extensions: ['.ts', '.tsx'],
+  },
+  {
+    id: 'css',
+    name: 'CSS',
+    extensions: ['.css'],
+  },
+  {
+    id: 'scss',
+    name: 'SCSS',
+    extensions: ['.scss'],
+  },
+  {
+    id: 'json',
+    name: 'JSON',
+    extensions: ['.json'],
+  },
+  {
+    id: 'graphql',
+    name: 'GraphQL',
+    extensions: ['.graphql'],
+  },
+  {
+    id: 'markdown',
+    name: 'Markdown',
+    extensions: ['.md', '.markdown'],
+  },
+  {
+    id: 'mdx',
+    name: 'MDX',
+    extensions: ['.mdx'],
+  },
+  {
+    id: 'html',
+    name: 'HTML',
+    extensions: ['.html', '.htm'],
+    embedded: ['css', 'babel'],
+  },
+  {
+    id: 'vue',
+    name: 'Vue',
+    extensions: ['.vue'],
+    embedded: ['css', 'scss', 'babel', 'babel-ts'],
+  },
+  {
+    id: 'yaml',
+    name: 'YAML',
+    extensions: ['.yaml', '.yml'],
+  },
+]
